4d2df52236509004bd9ae00436fa196931e20ace
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-02-11 14:27:10 +0600

    Added window.location to script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+let example1 = document.querySelector('#example1');
+example1.addEventListener('click', function(event){
+  window.location.href = "http://bing.com";
+
+
+
+  event.preventDefault();
+ 
+
+});
+let example2 = document.querySelector('#example2');
+example2.addEventListener('click', function(event){
+  window.location.href = "http://bing.com";
+
+  event.preventDefault();
+ 
+
+});
+let example3 = document.querySelector('#example3');
+example3.addEventListener('click', function(event){
+  window.location.href = "http://bing.com";
+
+  event.preventDefault();
+ 
+
+});
